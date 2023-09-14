d8fa449d4ba724e7e6b964c47ea92d4e84e748b3
TrebuchKill <bartwinski-hubert@web.de>
2023-09-14 19:14:14 +0200

    Refined Buildscript: if run, ensure drive0.qcow does not exist before running qemu-img

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
+const DRIVE0: &'static str = "drive0.qcow";
+
 fn main() -> std::io::Result<()>
 {
-    if !std::process::Command::new("qemu-img")
-        .args(["create", "-f", "qcow2", "drive0.qcow", "4G"])
-        .status()?.success()
+    let drive0 = std::path::PathBuf::from(DRIVE0);
+    if !drive0.exists()
     {
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "'qemu-img create' failed"));
+        println!("cargo:warning={} not found. Running qemu-img...", DRIVE0);
+        if !std::process::Command::new("qemu-img")
+            .args(["create", "-f", "qcow2", DRIVE0, "4G"])
+            .status()?.success()
+        {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "'qemu-img create' failed"));
+        }
     }
 
     Ok(())
